e359489c6adc8993f2b45024ccf79cb6d7c09e47
DanielCastilhoDIniz <danielcastilhodiniz@gmail.com>
2025-02-25 16:15:13 -0300

    feat(api-user) implementação dos tests e serializers

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -11,8 +11,8 @@ from rest_framework import status
 
 
 CREATE_USER_URL = reverse('user:create')
-TOKEN_URL = reverse('user:token')
-ME_URL = reverse('user:me')
+# TOKEN_URL = reverse('user:token')
+# ME_URL = reverse('user:me')
 
 
 def create_user(**params):
@@ -60,9 +60,10 @@ class PublicUserApiTests(TestCase):
             'name': 'Test Name',
         }
         res = self.client.post(CREATE_USER_URL, payload)
+
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
         user_exists = get_user_model().objects.filter(
             email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
-        self.assertNotIn('password', res.data)
