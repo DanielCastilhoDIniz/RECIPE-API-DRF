6283608e7f0a889f92afcf33461013df9095b1f3
DanielCastilhoDIniz <danielcastilhodiniz@gmail.com>
2025-03-04 23:32:56 -0300

    feat(doc) implementado gerenciamento de documentação

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -5,7 +5,7 @@ Serializers for the User API View.
 from django.contrib.auth import (
     get_user_model,
     authenticate,
-    )
+)
 from django.utils.translation import gettext as _
 
 from rest_framework import serializers
